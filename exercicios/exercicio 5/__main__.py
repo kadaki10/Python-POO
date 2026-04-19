234048d29e78b0f0610c120f14b13de6027e41f0
Reinaldo/python <reinaldo_997@hotmail.com>
2026-04-18 21:53:43 -0300

    ESTUDOS EM POO

---
+++ exercicios/exercicio 5/__main__.py
@@ -0,0 +1,16 @@
+from rich import print, inspect
+from classeexercicio5 import Aluno, Professor, Funcionario
+a1 = Aluno("José", 17, "Informatica", "T01")
+a1.fazer_aniversario()
+inspect(a1, methods= True)
+
+p1 = Professor("Luiz", 23, "Finanças", "Estagiario")
+a1.fazer_aniversario()
+inspect(p1, methods= True)
+
+
+
+f1 = Funcionario("Claudia", 27, "Secretaria", "Secretaria")
+f1.fazer_aniversario()
+f1.bater_ponto()
+inspect(f1, methods=True)

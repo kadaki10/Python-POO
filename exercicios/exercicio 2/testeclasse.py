cd45b0085f95814bcba9496c3aa1bea4ac5e3cc9
Reinaldo/python <reinaldo_997@hotmail.com>
2026-05-14 20:29:23 -0300

    ESTUDOS EM POO

---
+++ exercicios/exercicio 2/testeclasse.py
@@ -0,0 +1,30 @@
+class ContaBancaria:
+    """
+Cria uma conta bancaria e permite fazer saques e depositos
+    """
+    def __init__(self, id, nome, saldo = 0):
+        self.id = id
+        self.titular = nome
+        self.saldo = saldo
+
+    def __str__(self):
+        return f"A conta {self.id} de {self.titular} tem R${self.saldo} de saldo."
+
+
+    def depositar(self, valor):
+        self.saldo += valor
+        print(f"Deposito de {valor:,.2f} autorizado na conta {self.id} ")
+
+    def sacar(self, valor):
+        if valor > self.saldo:
+            print(f"SAQUE NEGADO! de R${valor:,.2f} na conta {self.id} SALDO INSUFICIENTE")
+        else:
+            self.saldo -= valor
+            print(f"Saque de R${valor:,.2f} autorizado na conta {self.id}.")
+
+c1 = ContaBancaria(112, "Reinaldo", 3000)
+c1.depositar(500)
+c1.sacar(2_000_000)
+print(c1)
+
+

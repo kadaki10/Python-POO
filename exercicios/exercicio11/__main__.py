685332e39c66efb9f0c45da777419c6318cbefc2
Reinaldo/python <reinaldo_997@hotmail.com>
2026-09-08 21:23:15 -0300

    ESTUDOS EM POO

---
+++ exercicios/exercicio11/__main__.py
@@ -0,0 +1,9 @@
+print(len("Gustavo Guanabara"))
+print(len(["Curso", "Python"]))
+print(len({"nome": "Maria", "idade": 22}))
+
+
+print(+12)
+print(5.5 + 2.2)
+print("Poli" + "Morfismo")
+print([1, 3, 4] + [9, 7])

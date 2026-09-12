81d54b4e3d1fc61ae247604e6a3040821d0277f4
Reinaldo/python <reinaldo_997@hotmail.com>
2026-09-11 22:03:00 -0300

    ESTUDOS EM POO

---
+++ exercicios/exercicio14/__main__.py
@@ -0,0 +1,8 @@
+from exercicios.exercicio14.classes import *
+
+def main():
+    x = Analisador()
+    x.analisar("Gustavo")
+
+if __name__ == "__main__":
+    main()
---
+++ exercicios/exercicio14/classes.py
@@ -0,0 +1,22 @@
+from functools import singledispatchmethod
+
+class Analisador:
+
+    @singledispatchmethod
+    def analisar(self, valor):
+        print(f"Não foi possivel analisar o {valor}")
+
+    @analisar.register
+    def _(self, valor: int):
+        print(f"{valor} é um número inteiro")
+
+    @analisar.register
+    def _(self, valor: float):
+        print(f"{valor} é um número flutuante (real)")
+
+    def _(self, valor: str):
+        print(f"{valor} é uma cadeia de caracteres")
+
+    @analisar.register
+    def _(self, valor: tuple|list|dict):
+        print(f"{valor} é uma coleção de dados")
